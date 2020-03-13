d582d157891d87d8d4e4b2db350795db9366d7a6
Vladislav2147 <vandl3511@gmail.com>
2020-03-13 15:00:06 +0300

    created some data classes

---
+++ CourseWorkClasses/CourseWorkClasses/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWorkClasses
+{
+	enum ProductType { 
+		PC, VideoCard, RAM, Processor, MotherBoard, HardDrive, PowerSupply, Case, Other
+	}
+
+}
